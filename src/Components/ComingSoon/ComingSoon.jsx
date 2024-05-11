1cb8da57f02d825906d456359dafd89e506f5775
Gauravasawa <asawagaurav3@gmail.com>
2024-05-11 17:34:48 +0530

    added comming soon component

---
+++ src/Components/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,12 @@
+const ComingSoon = () => {
+  return (
+    <div className="container mx-auto py-10 text-center">
+      <h1 className="text-4xl font-bold mb-6">Coming Soon!</h1>
+      <p className="text-lg mb-8">
+        This page is under construction. Stay tuned!
+      </p>
+    </div>
+  );
+};
+
+export default ComingSoon;
